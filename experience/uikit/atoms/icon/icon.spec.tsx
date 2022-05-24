ea182eeacbd94b43711933449168439b911f495d
Hrach <steepgaskin@gmail.com>
2022-05-24 13:48:48 +0400

    icon-update

--- experience/uikit/atoms/icon/icon.spec.tsx
+++ experience/uikit/atoms/icon/icon.spec.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { BasicIcon } from './icon.composition';
 
 it('should render with the correct text', () => {
-  const { getByText } = render(<BasicIcon />);
   const rendered = true;
   expect(rendered).toBeTruthy();
 });
